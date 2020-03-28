23cc35de0bb70a1ed92d31fb24bd870123b78751
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-03-28 12:49:10 +0100

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+newlist = [] # empty list 
+# append adds something at the end of the list 
+newlist.append(1)
+newlist.append(2)
+print(newlist) #this will print the list 
+print(newlist[0]) #this accesses specific elements of the list 
