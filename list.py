512db596a37d0fe43bb5f8e8cd525fe6b5a1a10b
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-03-30 12:55:07 +0200

    Update list.py

--- list.py
+++ list.py
@@ -4,3 +4,7 @@ newlist.append(1)
 newlist.append(2)
 print(newlist) #this will print the list 
 print(newlist[0]) #this accesses specific elements of the list 
+
+for x in mylist: #this will print all the elements of the list since x signifies every element 
+    print(x)
+    
