165a3ef3114e3624f313dfcb3a90d1f4be65a9d5
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-03-30 13:06:02 +0200

    Update list.py

--- list.py
+++ list.py
@@ -2,6 +2,7 @@ newlist = [] # empty list
 # append adds something at the end of the list 
 newlist.append(1)
 newlist.append(2)
+imp_number = newlist[1] # will assign 2 to imp_number
 print(newlist) #this will print the list 
 print(newlist[0]) #this accesses specific elements of the list 
 
